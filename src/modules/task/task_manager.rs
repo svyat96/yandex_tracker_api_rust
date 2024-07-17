9141402e166eba2494902f47e6218df2de0353f7
Svyatoslav Sporykhin <svyat1996@gmail.com>
2024-07-17 16:49:08 +0300

    feat(PROJMGMT-9): Add RustDoc comments and implement task batch processing
    - Added detailed RustDoc comments to TaskBatchHandler and its methods.
    - Implemented process_tasks method to handle task creation and subtask processing.
    - Included save_task_batch method to save the current state of task batches to a JSON file.

--- src/modules/task/task_manager.rs
+++ src/modules/task/task_manager.rs
@@ -6,33 +6,46 @@ use super::CreatedTask;
 
 use tokio::time::{sleep, Duration};
 
+/// Handles batch processing of tasks using the Yandex Tracker API client.
 pub struct TaskBatchHandler {
+    /// The API client used for interacting with the Yandex Tracker.
     api_client: TaskApiClient,
 }
 
 impl TaskBatchHandler {
+    /// Creates a new `TaskBatchHandler` instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `api_client` - The API client used for task operations.
     pub fn new(api_client: TaskApiClient) -> Self {
         TaskBatchHandler { api_client }
     }
 
+    /// Processes a batch of tasks, creating tasks and their subtasks in the Yandex Tracker.
+    ///
+    /// # Arguments
+    ///
+    /// * `task_batch` - A batch of tasks to be processed.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating the success or failure of the operation.
     pub async fn process_tasks(&self, task_batch: TaskBatch) -> Result<(), HandleResponseError> {
         let mut mut_task_batch = task_batch.clone();
 
-        while mut_task_batch.created.is_empty() == false {
-            let vec_created: Vec<CreatedTask> =
-                mut_task_batch.created.clone().into_iter().collect();
+        while !mut_task_batch.created.is_empty() {
+            let vec_created: Vec<CreatedTask> = mut_task_batch.created.clone().into_iter().collect();
 
             match vec_created.first() {
                 Some(task) => {
-                    let response = self.api_client.create_task(&task).await?;
+                    let response = self.api_client.create_task(task).await?;
                     sleep(Duration::from_secs(1)).await;
 
                     let subtask = task.subtasks.clone();
                     mut_task_batch.created.remove(task);
                     for task in subtask {
-                        mut_task_batch
-                            .created
-                            .insert(task.set(response.key.clone()));
+                        mut_task_batch.created.insert(task.set(response.key.clone()));
                     }
                     self.save_task_batch(&mut_task_batch)?;
                 }
@@ -40,9 +53,18 @@ impl TaskBatchHandler {
             }
         }
 
-        return Ok(());
+        Ok(())
     }
 
+    /// Saves the current state of the task batch to a JSON file.
+    ///
+    /// # Arguments
+    ///
+    /// * `task_batch` - The task batch to be saved.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating the success or failure of the operation.
     fn save_task_batch(&self, task_batch: &TaskBatch) -> Result<(), std::io::Error> {
         let tasks_json = serde_json::to_string_pretty(task_batch)?;
         fs::write("tasks.json", tasks_json)?;
