275db25785348f1ed77a601221b3ebff160477b7
Svyatoslav Sporykhin <svyat1996@gmail.com>
2024-07-26 16:27:51 +0300

    PROJMGMT-31:
    - Removed the custom deserializer for the `queue` field in `CreatedTaskInfo`

--- src/modules/task/error_response.rs
+++ src/modules/task/error_response.rs
@@ -10,10 +10,27 @@ pub struct ErrorResponse {
     /// This field is serialized/deserialized as `errorMessages`.
     #[serde(rename = "errorMessages")]
     pub error_messages: Vec<String>,
+
+    /// An optional field capturing specific error details.
+    ///
+    /// This field is serialized/deserialized as `errors`.
+    #[serde(rename = "errors")]
+    pub errors_model: Option<ErrorsModel>,
     
     /// The status code of the error response.
     ///
     /// This field is serialized/deserialized as `statusCode`.
     #[serde(rename = "statusCode")]
     pub status_code: u32,
+}
+
+/// Represents detailed error information in the API response.
+///
+/// This struct captures specific error details related to the API response.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ErrorsModel {
+    /// An optional field representing the priority of the error.
+    ///
+    /// This field can be `None` if no priority information is provided.
+    pub priority: Option<String>,
 }
